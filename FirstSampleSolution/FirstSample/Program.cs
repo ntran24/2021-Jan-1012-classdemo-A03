3361050a6d7aa46b09b11443e5c2833f99f6c982
ntran24 <77078988+ntran24@users.noreply.github.com>
2021-01-13 10:19:06 -0700

    First sample console program

---
+++ FirstSampleSolution/FirstSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FirstSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // the whiteline() command will output the 
+            // the to  the console window.
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
