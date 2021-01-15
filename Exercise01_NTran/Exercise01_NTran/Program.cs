1e961f031921d630a092652cff286a8aa690626c
ntran24 <77078988+ntran24@users.noreply.github.com>
2021-01-15 11:41:06 -0700

    The First exercise in class

---
+++ Exercise01_NTran/Exercise01_NTran/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise01_NTran
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Output the program that describle myself
+            // .WriteLine() Which automaticaly goes the next line.
+            //I will tell you about myself
+            //My favorite thing to do
+            //What I am expected
+            Console.WriteLine($"Hello everyone, my name is Nicolas Tran and I am in the DMIT (Computer Software Development Concentration).\n");
+
+
+            Console.WriteLine($"I am really enjoy playing Soccer and It is my favorite.\n");
+
+            Console.WriteLine($"I really interested in programming course, and I will work hard in order to get high grade.\n");
+
+
+        }
+    }
+}
