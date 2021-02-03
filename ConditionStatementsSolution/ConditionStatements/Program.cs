364c0ee6288d40e8b0f68f605cf7f1a9f701ff67
ntran24 <77078988+ntran24@users.noreply.github.com>
2021-02-03 10:46:12 -0700

    In class exercise

--- ConditionStatementsSolution/ConditionStatements/Program.cs
+++ ConditionStatementsSolution/ConditionStatements/Program.cs
@@ -6,10 +6,10 @@ namespace ConditionStatements
     {
         static void Main(string[] args)
         {
-           // const int MIN_NUMBER = 1;
-           // const int MAX_NUMBER = 9;
+            // const int MIN_NUMBER = 1;
+            // const int MAX_NUMBER = 9;
 
-           // Random keyrad = new Random();
+            // Random keyrad = new Random();
 
             //int number1 = keyrad.Next(MIN_NUMBER, MAX_NUMBER + 1);
             //int number2 = keyrad.Next(MIN_NUMBER, MAX_NUMBER + 1);
@@ -28,24 +28,50 @@ namespace ConditionStatements
             //Console.WriteLine($"{number1} + {number2} = {answer} Your answer is {(number1 + number2) == answer}");
 
             //the one way if statement
-            double radius = 4.5;
-            if (radius > 0)
-            {
-                double area = Math.Pow(radius, Math.PI);
-                if (area >= 100.0)
-                {
-                    Console.WriteLine($"The are of a circle with a radius of {radius} is {Math.Round(area, 2)}. Your circle is large enough. ");
-                }
-                else
-                {
-                    Console.WriteLine($"The are of a circle with a radius of {radius} is {Math.Round(area, 2)}. Increase the size of the radius");
-                }
-                
-            }
+            //double radius = 4.5;
+            // if (radius > 0)
+            // {
+            //    double area = Math.Pow(radius, Math.PI);
+            //    if (area >= 100.0)
+            //   {
+            //       Console.WriteLine($"The are of a circle with a radius of {radius} is {Math.Round(area, 2)}. Your circle is large enough. ");
+            //    }
+            //    else
+            //    {
+            //       Console.WriteLine($"The are of a circle with a radius of {radius} is {Math.Round(area, 2)}. Increase the size of the radius");
+            //    }
+            //    
+            // }
 
             //A two-way if statement 
             //has a true path and a false path
             //the ture
+
+            int FahrenheitTemperature = 100;
+            int CelsiusTemperature = 0;
+
+            CelsiusTemperature = (FahrenheitTemperature - 32) * (5 / 9);
+
+            if (CelsiusTemperature < 0)
+            {
+                Console.WriteLine("It is freezing out!");
+            }
+            else if(CelsiusTemperature < 15)
+            {
+                Console.WriteLine("Wear a jacket");
+            }
+            else if(CelsiusTemperature < 30)
+            {
+                Console.WriteLine("It is a loving day");
+            }
+            else
+            {
+                Console.WriteLine("It is finally summer");
+            }
+            
+
+
+            
 
 
         }
